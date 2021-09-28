f8ce22486b8e27b1ee39e25ac575c73898c6b965
salvatore <salvatore.didio@gmail.com>
2021-09-28 12:34:08 +0200

    add status barre

--- src/components/Scanner/index.js
+++ src/components/Scanner/index.js
@@ -1,42 +1,19 @@
 import SetupControls from "../Controls";
 import Video from "../Video";
 
-/*
-const Scanner = (function () {
+function _Scanner() {
+  this.isScanning = false;
 
-  let self;
-
-  function startVideo() {
-    Video.launchCamera();
-    self.controls.scanBtn.style.display = "none";
-  }
-
-  function startApp() {
-    self = this;
-    self.controls = SetupControls(self);
-  }
-
-  return {
-    startApp, startVideo
+  this.startVideo = function () {
+    if (!this.isScanning) {
+      Video.launchCamera();
+    }
   };
 
-})();
-
-export default Scanner;
-
-*/
-
-function _Scanner () {
-    this.startVideo = function () {
-        console.log(this);
-        Video.launchCamera();
-      }
-
-    this.startApp = function () {
-       SetupControls(this);
-    }
+  this.startApp = function () {
+    SetupControls(this);
+  };
 }
 
 const Scanner = new _Scanner();
 export default Scanner;
-
